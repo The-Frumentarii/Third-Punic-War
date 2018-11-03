9701aaf8dd74197bc42dd136e7c367d4d822b525
aoifekeane <40514635+aoifekeane@users.noreply.github.com>
2018-11-03 15:24:23 +0000

    Update carthage.c
    - added the binary/decimal conversion bits
    - also have a helper opcode getter-thing which works fro how i imagine everything will go down, but throw a cursory eye over it and we can decide if thats what we want to do i guess
    -everything works: is it exceptionally pretty? not even slightly, but she gets the job done

--- carthage.c
+++ carthage.c
@@ -25,9 +25,10 @@ void and();
 void or();
 
 //Helper functions:
-void cdtb();	//(Convert decimal to binary)   DONE 
-void cbtd();	//(Convert binary to decimal)   DONE 
-
+char* cdtb(int p, char *binary);	//(Convert decimal to binary)   DONE 
+int cbtd(char *str);	//(Convert binary to decimal)   DONE 
+int opcodeM(char *str)	//gets the opcode DONE 
+	
 void display_memory_content();	//????
 void convert_to_assembly();
 
@@ -74,4 +75,76 @@ void read_file(){
 }
 void fde(){
 	printf("Executed the stuff in memory.\n");
+}
+//takes in a binary number as a string and returns the decimal value as an int
+int cbtd(char *str){
+	int p, i, j, k, n;
+	n = strlen(str);
+	int binary[n];
+	for(i = 0; i<n; i++){
+		binary[i] = str[i] - '0';
+	}
+	k = 1;
+	if(binary[0] == 1){
+		for(j = 0; j<n; j++){
+			k*=2;
+		}
+		p -= k;
+		
+	}
+	else{
+		p =0;
+	}
+	for(i=1; i<n; i++){
+		k=1;
+		if(binary[i] ==1){
+			
+			for(j = 0; j<n-(1+i); j++){
+				k*=2;
+			}
+		}
+		else{
+			k = 0;
+		}
+	p+=k;
+	}
+	
+	return p;
+}
+//takes in a decimal number and the address of a string to return the binary value as a string
+char* cdtb(int p, char *binary){
+	int i;
+	
+	if(p<0){
+		p += 2048;
+		binary[0] = '1';
+		
+	}
+	else{
+		binary[0] = '0';
+	}
+	for(i=1; i<12; i++){
+		if(p%2==0){
+			binary[12-i] = '0';
+		}
+		else{
+			binary[12-i] = '1';
+		}
+	p/=2;
+	}
+	binary[12] = '\0';
+	return binary;
+
+}
+//this gets the opcode from an instruction- decided to return it as an int bc it means we can use a switch on it later
+int opcodeM(char *str){
+	int i = 0;
+	char opcode[5];
+	for(i=0; i<4; i++){
+		opcode[i] = str[i];
+	}
+	opcode[4] = '\0';
+	
+	
+	return cbtd(opcode);
 }
