685eedca7af3108ca3ac793c0c812a355840b27c
LunguRares <44687597+LunguRares@users.noreply.github.com>
2018-11-06 13:59:47 +0000

    Update carthage.c

--- carthage.c
+++ carthage.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
-//List of functions we need to implement: (Might need to make more helper functions; Return types and parameters not included) 
+//List of functions we need to implement: (Might need to make more helper functions; Return types and parameters not included)
 void load_default();
 void read_console_input();
 void read_file();
-void fde();       // fetch-decode-execute  
+void fde();       // fetch-decode-execute
 
 //This part contains functions for the operations of our disassembler:
 void load();
@@ -25,31 +25,33 @@ void and();
 void or();
 
 //Helper functions:
-char* cdtb(int p, char *binary);	//(Convert decimal to binary)   DONE 
-int cbtd(char *str);	//(Convert binary to decimal)   DONE 
-int opcodeM(char *str)	//gets the opcode DONE 
-	
+char* cdtb(int p, char *binary);	//(Convert decimal to binary)   DONE
+int cbtd(char *str);	//(Convert binary to decimal)   DONE
+int opcodeM(char *str)	//gets the opcode DONE
+
 void display_memory_content();	//????
 void convert_to_assembly();
 
 
 
-int main(int argc, char* argv[]){ 
+int main(int argc, char* argv[]){
+
+	short memory[4096];
 
 	if(argc<2){
 		printf("Missing argument(s).\n");
 		printf("Program terminated.\n");
 		return 1;
 	}
-	
-	if(!(strcmp(argv[1],"-d")))		
-		load_default();		//load default content to memory 	
+
+	if(!(strcmp(argv[1],"-d")))
+		load_default();		//load default content to memory
 	else{
 		if(!(strcmp(argv[1],"-c")))
-			read_console_input();		//read user input from console into the memory 
+			read_console_input();		//read user input from console into the memory
 		else{
 			if(!(strcmp(argv[1],"-f")))
-				read_file();		//read the contents of a file into memory 
+				read_file();		//read the contents of a file into memory
 			else{
 				printf("Invalid argument.\n");
 				printf("Program terminated.\n");
@@ -57,11 +59,11 @@ int main(int argc, char* argv[]){
 			}
 		}
 	}
-			
 
-	fde(); // fetch-decode-execute  	
+
+	fde(); // fetch-decode-execute
 	return 0;
-	
+
 }
 
 void load_default(){
@@ -90,7 +92,7 @@ int cbtd(char *str){
 			k*=2;
 		}
 		p -= k;
-		
+
 	}
 	else{
 		p =0;
@@ -98,7 +100,7 @@ int cbtd(char *str){
 	for(i=1; i<n; i++){
 		k=1;
 		if(binary[i] ==1){
-			
+
 			for(j = 0; j<n-(1+i); j++){
 				k*=2;
 			}
@@ -108,17 +110,17 @@ int cbtd(char *str){
 		}
 	p+=k;
 	}
-	
+
 	return p;
 }
 //takes in a decimal number and the address of a string to return the binary value as a string
 char* cdtb(int p, char *binary){
 	int i;
-	
+
 	if(p<0){
 		p += 2048;
 		binary[0] = '1';
-		
+
 	}
 	else{
 		binary[0] = '0';
@@ -144,7 +146,7 @@ int opcodeM(char *str){
 		opcode[i] = str[i];
 	}
 	opcode[4] = '\0';
-	
-	
+
+
 	return cbtd(opcode);
 }
